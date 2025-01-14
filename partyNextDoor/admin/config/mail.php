e1335ade6bc5ab6cee13b837e97109344e5e3f24
kuaix2 <shuhan.chuang0@gmail.com>
2025-01-14 14:52:14 +0100

    fix cred leakage

--- partyNextDoor/admin/config/mail.php
+++ partyNextDoor/admin/config/mail.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_username' => 'shuhan.chuang0@gmail.com',
-    'smtp_password' => 'ulnc vkkf hdpv jiiy',
+    'smtp_username' => 'uremailhere@gmail.com',
+    'smtp_password' => 'xxxx xxxx xxxx xxxx',
     'smtp_port' => 587,
     'from_name' => 'PartyNextDoor Support'
 ]; 
