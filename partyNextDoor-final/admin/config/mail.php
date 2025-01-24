0edb7a21425f61c0a8cf5ffb26daadb44b28f5f8
allan-benard <allandu97435@gmail.com>
2025-01-24 11:53:44 +0100

    mail

--- partyNextDoor-final/admin/config/mail.php
+++ partyNextDoor-final/admin/config/mail.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'smtp_host' => 'smtp.gmail.com',
-    'smtp_username' => 'allandu97435@gmail.com',
-    'smtp_password' => 'movt wrcq efje zejk',
+    'smtp_username' => '6newdev@gmail.com',
+    'smtp_password' => 'dxuv xowi iogg owmf',
     'smtp_port' => 587,
     'from_name' => 'PartyNextDoor Support'
 ]; 
